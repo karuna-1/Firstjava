047fc585de6004e84287f3eb9880925f3c87440c
karuna-1 <kamargolmath@gmail.com>
2026-01-21 12:10:38 +0530

    added

---
+++ calculator/src/main/java/calculator/Addition.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import java.util.Scanner; // Import the Scanner class
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.println("The sum of the numbers is: " + sum);
+
+        scanner.close();
+    }
+}
